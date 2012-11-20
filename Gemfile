536d07deef7158038ca33a31b8c87a96e1f6b792
Phu Phan <Phu_Phan@playstation.sony.com>
2012-11-20 10:33:37 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'thin'
+gem 'database_cleaner', '>= 0.8.0'
+gem 'simple_form'
+gem "nested_form"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GadgifyClient::Application.config.secret_token = '2ca5e3bd93e3f6904b501a2160718e8f7ef9bf28438e485654113290b572b528ea64d41646f94ba6753628f948b7afb09de35b627b9c75259309f143c82fedf4'
